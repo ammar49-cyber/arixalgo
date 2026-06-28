f3a1331be2fa2df5b2bb2852494a4a090207cd9c
ARIX-Algo <algoarix@gmail.com>
2026-06-27 21:38:21 -0700

    restructure: remove old src/ tree
    All sources now live under:
    - include/arix/ (headers)
    - kernel/ (core runtime)
    - algorithms/ (HSS, SER, ARC, NPE, FM)
    - security/ (crypto, obfuscation)
    
    Old src/ directories tracked by git have been removed.

--- src/distributed/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub fn version() -> &'static str {
-    "0.1.0"
-}
-
-pub async fn hello() -> String {
-    format!("Hello from ARIX-Distributed v{}", version())
-}
--- src/python/arix_algo/__init__.py
+++
@@ -1,5 +0,0 @@
-from .tensor import Tensor
-from .model import Model
-from .train import Trainer
-
-__version__ = "0.1.0"
